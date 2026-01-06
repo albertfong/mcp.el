6a633f1871c520a0b7e73e29a6ba5cb2d9821f1f
Albert Fong <contact@albertfong.name>
2026-01-05 18:54:00 -0800

    Bugfix: needs to be a proper plist

--- mcp.el
+++ mcp.el
@@ -1010,9 +1010,9 @@ the response to extract and return text content."
            :name tool-name
            :async asyncp
            :description description
+           :confirm confirm
            :args
-           (mcp--parse-tool-args properties (or required '()))
-           (when confirm :confirm t)))))))
+           (mcp--parse-tool-args properties (or required '()))))))))
 
 (defun mcp--set-log-level (connection log-level syncp)
   "Helper function to set the log level for the MCP server.
